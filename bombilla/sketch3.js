1db54033b2df3a952abfe3f55e83320f8d06f498
jonimelavo <43881219+jonimelavo@users.noreply.github.com>
2018-11-28 13:52:35 +0100

    Update sketch3.js

--- bombilla/sketch3.js
+++ bombilla/sketch3.js
@@ -1,36 +1,49 @@
+var width = 0;
+var height = 0;
+
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(2100, 1100);
+ 
 }
 
+
 function draw() {
-  background(102);
+  background(0);
+  width = width + 50;
+  if (width > 2100) {
+   width = 0;
+  
 
+}
+height = height + 10;
+  if (height > 1100) {
+   height = 0;
+  
+
+}
+
+
+ 
   push();
-  translate(width*0.2, height*0.5);
-  rotate(frameCount / 200.0);
-  polygon(0, 0, 82, 3);
+  translate(width*1, height*1);
+  rotate(frameCount / 2.0);
+  translate(p5.Vector.fromAngle(millis() / 100, 50));
+  polygon(0, 0, 100, 3);
   pop();
 
-  push();
-  translate(width*0.5, height*0.5);
-  rotate(frameCount / 50.0);
-  polygon(0, 0, 80, 20);
-  pop();
-
-  push();
-  translate(width*0.8, height*0.5);
-  rotate(frameCount / -100.0);
-  polygon(0, 0, 70, 7);
-  pop();
+  
 }
 
 function polygon(x, y, radius, npoints) {
   var angle = TWO_PI / npoints;
   beginShape();
   for (var a = 0; a < TWO_PI; a += angle) {
+    
     var sx = x + cos(a) * radius;
     var sy = y + sin(a) * radius;
     vertex(sx, sy);
   }
+  
   endShape(CLOSE);
+  
 }
